647e2228f1037ad69d4d240cf583562a649ff6cf
q2043246 <q2043246@tees.ac.uk>
2024-10-17 12:21:15 +0100

    Add Trainer Controller

---
+++ Vista.Api/Controllers/TrainersController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Threading.Tasks;
+using Humanizer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using Vista.Api.Data;
+
+namespace Vista.Api.Controllers
+{
+    [Route("api/[controller]")] //Sets the URL route to api/trainers, using the controller's name (TrainerController)
+    [ApiController] //Marks the class as an API Controller, enabling features like automatic model validation and consistent HTTP responses.
+    public class TrainersController : ControllerBase  //The TrainersController class handles HTTP requests related to "trainers"
+                                                      //and inherits core API functionality from ControllerBase
+    {
+        private readonly TrainersDbContext _context;  // The TrainersDbContext represents the session with the
+                                                      // database and allows the controller to interact with the
+                                                      // database(e.g., querying, adding, updating, and
+                                                      // deleting records). It is initialised in the constructor.
+
+        public TrainersController(TrainersDbContext context) //This is the constructor for the Controller class and
+        {                                                    //takes a TrainersDbContext parameter.
+            _context = context;                              // Assigns the TrainersDbContext parameter to the private
+        }                                                    // field _context.This allows the controller to use _context
+                                                             // in its methods to interact with the database.
+
+
+        // GET: api/Trainers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Trainer>>> GetTrainers()
+        {
+            return await _context.Trainers.ToListAsync();
+        }
+
+        // GET: api/Trainers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Trainer>> GetTrainer(int id)
+        {
+            var trainer = await _context.Trainers.FindAsync(id);
+
+            if (trainer == null)
+            {
+                return NotFound();
+            }
+
+            return trainer;
+        }
+
+        // PUT: api/Trainers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTrainer(int id, Trainer trainer)
+        {
+            if (id != trainer.TrainerId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(trainer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TrainerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Trainers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Trainer>> PostTrainer(Trainer trainer)
+        {
+            _context.Trainers.Add(trainer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTrainer", new { id = trainer.TrainerId }, trainer);
+        }
+
+        // DELETE: api/Trainers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTrainer(int id)
+        {
+            var trainer = await _context.Trainers.FindAsync(id);
+            if (trainer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Trainers.Remove(trainer);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TrainerExists(int id)
+        {
+            return _context.Trainers.Any(e => e.TrainerId == id);
+        }
+    }
+}
