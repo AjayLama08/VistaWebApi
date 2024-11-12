1ffd686557de346f1e7a80b093670219ab3f4a2e
AJAY LAMA <Q2043246@live.tees.ac.uk>
2024-11-12 14:18:51 +0000

    Need to fix the Workshop and WorkshopParticipants

--- Vista.Web/Pages/Workshops/Index.cshtml.cs
+++ Vista.Web/Pages/Workshops/Index.cshtml.cs
@@ -4,25 +4,52 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Vista.Web.Data;
+using Vista.Web.Services;
+using Vista.Web.ViewModels;
 
 namespace Vista.Web.Pages.Workshops
 {
     public class IndexModel : PageModel
     {
         private readonly Vista.Web.Data.WorkshopsContext _context;
+        private readonly CategoryService _categoryService;
 
-        public IndexModel(Vista.Web.Data.WorkshopsContext context)
+        // Empty SelectListItem List for category drop down
+        public List<SelectListItem> CategoryItems { get; set; } = [];
+
+        //Page model constructor with database context and category service passes in via DI (dependency injection)
+        public IndexModel(Vista.Web.Data.WorkshopsContext context, CategoryService categoryService)
         {
             _context = context;
+            _categoryService = categoryService;
         }
 
-        public IList<Workshop> Workshop { get;set; } = default!;
+        public IList<WorkshopVM> Workshop { get;set; } = default!;
 
         public async Task OnGetAsync()
         {
-            Workshop = await _context.Workshops.ToListAsync();
+            // Get list of Categories from service
+            var catList = await _categoryService.GetCategoryItemsAsync();
+
+            // Get workshops from data base via EF
+            var workshops = await _context.Workshops.ToListAsync();
+
+            // Convert data model to view model with category name
+            // This de-couples the UI from the database
+            Workshop = workshops.Select(w => new WorkshopVM
+            {
+                WorkshopId = w.WorkshopId,
+                Name = w.Name,
+                DateAndTime = w.DateAndTime,
+                BookingRef = w.BookingRef,
+                CategoryCode = w.CategoryCode,
+                CategoryName = catList.FirstOrDefault(c => c.CategoryCode == w.CategoryCode)?
+                                    .CategoryName ?? w.CategoryCode
+            })
+            .ToList();
         }
     }
 }
---
+++ Vista.Web/ViewModels/WorkshopVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vista.Web.ViewModels
+{
+    public class WorkshopVM
+    {
+        public int WorkshopId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime DateAndTime { get; set; }
+
+        [Required]
+        public string CategoryCode { get; set; } = string.Empty;
+        public string CategoryName { get; set; } = string.Empty;
+        public string? BookingRef { get; set; }
+    }
+}
